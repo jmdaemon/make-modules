7432e0e6f2db11828280c7837eccf5c7d5170d69
Joseph Diza <josephm.diza@gmail.com>
2022-04-11 20:41:26 -0700

    Add install/uninstall rules

--- install.mk
+++ install.mk
@@ -2,3 +2,18 @@
 ifeq ($(PREFIX),)
     PREFIX := /usr/local
 endif
+
+# Installs both debug and release candidate
+# for a binary and library
+
+#
+# Install / Uninstall
+#
+
+.PHONY: install uninstall
+
+# Install both targets
+install: install-bin install-lib
+
+# Uninstall both targets
+uninstall:  uninstall-bin uninstall-lib
