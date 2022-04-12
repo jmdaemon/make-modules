7be0cdcaec43bbe2ec9a30fb2ccb9f3db5247e59
Joseph Diza <josephm.diza@gmail.com>
2022-04-11 20:57:57 -0700

    Add structure make module

---
+++ structure.mk
@@ -0,0 +1,23 @@
+# Initialize the project structure
+
+#
+# Project Structure
+#
+
+# These are used to generate the build structure:
+# - src
+# - test
+# - include
+# - subprojects
+# - build
+# - build/{debug, release}
+# - build/{debug, release}/lib/
+# - build/{debug, release}/bin/
+
+PATHS = src
+PATHT = test
+PATHB = build
+PATHI = include
+
+SUBPROJECTS = subprojects
+
