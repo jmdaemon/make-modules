a9c58974b530bb4968df0863765405dc8571a1e5
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 01:00:35 -0700

    Add docs make module

---
+++ docs.mk
@@ -0,0 +1,16 @@
+#
+# Documentation
+# 
+
+DOC_HTML = build/docs/html
+DOC_SRC = index.html
+DOC_OBJ = $(DOC_HTML)/$(DOC_SRC)
+
+docs: $(DOC_OBJ)
+
+$(DOC_OBJ): $(EXE_DEPS)/*.o $(PATHS)/*.c $(PATHI)/*.h
+	@echo Building Docs
+	@doxygen
+
+clean-docs:
+	$(RMDIR) build/docs
