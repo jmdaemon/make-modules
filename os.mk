2b6ec30c8f452b6ad514a9e1c749e05852182b25
Joseph Diza <josephm.diza@gmail.com>
2022-08-02 19:54:34 -0700

    Create platform independent COPY command

--- os.mk
+++ os.mk
@@ -12,9 +12,11 @@ ifeq ($(OS),Windows_NT)
   ifeq ($(shell uname -s),) # not in a bash-like shell
 	CLEANUP = del /F /Q
 	MKDIR = mkdir
+	COPY = Xcopy /E /I
   else # in a bash-like shell, like msys
 	CLEANUP = rm -f
 	MKDIR = mkdir -p
+	COPY = cp -rf
   endif
 	TARGET_EXTENSION=exe
 	SHARED_LIBRARY_EXT=dll
@@ -22,6 +24,7 @@ ifeq ($(OS),Windows_NT)
 else
 	CLEANUP = rm -f
 	MKDIR = mkdir -p
+	COPY = cp -rf
 	TARGET_EXTENSION=out
 	SHARED_LIBRARY_EXT=so
 	STATIC_LIBRARY_EXT=a
