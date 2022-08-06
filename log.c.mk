bd545910e740cc0e9aa1d48c243436dac765c256
Joseph Diza <josephm.diza@gmail.com>
2022-08-06 15:03:10 -0700

    Build static libraries in log.c

--- log.c.mk
+++ log.c.mk
@@ -5,8 +5,10 @@ SP_LOG_C_SRC 			= $(SUBPROJECTS)/$(SP_LOG_C_NAME)/src
 SP_LOG_C_OBJS 		= $(SP_LOG_C_SRCS:.c=.o)
 SP_LOG_C_INCLUDES = -I$(SP_LOG_C_SRC)
 
+# Build dynamic, static libraries
 SP_LOGC_LIB_NAME 	= logc
 SP_LOGC_LIB 			= $(SP_LOGC_DIR)/lib$(SP_LOGC_LIB_NAME).so
+SP_LOGC_STATIC 		= $(SP_LOGC_DIR)/lib$(SP_LOGC_LIB_NAME).a
 
 .PHONY: logc clean-logc
 
@@ -20,11 +22,10 @@ SP_LOGC_CFLAGS = $(SP_LOG_C_INCLUDES)
 SP_SOURCES 	+= $(SP_LOGC_SRCS)
 SP_DEPENDS 	+= $(SP_LOGC_OBJS)
 SP_INCLUDES += $(SP_LOG_C_INCLUDES)
+SP_LIBS 		+= $(SP_LOGC_LIB)
 
-SP_LIBS += $(SP_LOGC_LIB)
-
-# Create the directory, object files, and library
-logc: $(SP_LOGC_DIR) $(SP_LOGC_OBJS) $(SP_LOGC_LIB)
+# Create the directory, object files, dynamic & static libraries
+logc: $(SP_LOGC_DIR) $(SP_LOGC_OBJS) $(SP_LOGC_LIB) $(SP_LOGC_STATIC)
 
 # Builds build/depends/log.c/log.o, depends on subproject srcs
 $(SP_LOGC_OBJS): $(SP_LOGC_SRCS)
@@ -34,6 +35,10 @@ $(SP_LOGC_OBJS): $(SP_LOGC_SRCS)
 # Create dynamic library in build/depends/log.c/liblog.so
 $(SP_LOGC_LIB): $(SP_LOGC_OBJS)
 	$(CC) -shared $(SP_INCLUDES) -o $@ $^
+
+# Create static library in build/depends/log.c/liblog.so
+$(SP_LOGC_STATIC): $(SP_LOGC_OBJS)
+	ar rcs $@ $^
 
 $(SP_LOGC_DIR):
 	$(MKDIR) $(SP_LOGC_DIR)
