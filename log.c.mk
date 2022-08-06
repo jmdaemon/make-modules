05fe2ff8daf05778f9d4bef06e8f1dea0c0526b6
Joseph Diza <josephm.diza@gmail.com>
2022-08-06 16:29:31 -0700

    Make output messager easier to understand

--- log.c.mk
+++ log.c.mk
@@ -1,9 +1,9 @@
 # Variables
 SP_LOG_C_NAME 		= log.c
 SP_LOG_C_SRCS 		= log.c
-SP_LOG_C_SRC 			= $(SUBPROJECTS)/$(SP_LOG_C_NAME)/src
+SP_LOG_C_SRCD			= $(SUBPROJECTS)/$(SP_LOG_C_NAME)/src
 SP_LOG_C_OBJS 		= $(SP_LOG_C_SRCS:.c=.o)
-SP_LOG_C_INCLUDES = -I$(SP_LOG_C_SRC)
+SP_LOG_C_INCLUDES = -I$(SP_LOG_C_SRCD)
 
 # Build dynamic, static libraries
 SP_LOGC_LIB_NAME 	= logc
@@ -15,7 +15,7 @@ SP_LOGC_STATIC 		= $(SP_LOGC_DIR)/lib$(SP_LOGC_LIB_NAME).a
 # Log.c Dependency
 # SP_LOGC_DIR: Outputs build dependencies to build/{debug,release}/depends/subproject_name
 SP_LOGC_DIR 	 = $(TARGET_DIR)/depends/$(SP_LOG_C_NAME)
-SP_LOGC_SRCS 	 = $(addprefix $(SP_LOG_C_SRC)/, $(SP_LOG_C_SRCS))
+SP_LOGC_SRCS 	 = $(addprefix $(SP_LOG_C_SRCD)/, $(SP_LOG_C_SRCS))
 SP_LOGC_OBJS 	 = $(addprefix $(SP_LOGC_DIR)/, $(SP_LOG_C_OBJS))
 SP_LOGC_CFLAGS = $(SP_LOG_C_INCLUDES)
 
@@ -30,19 +30,18 @@ logc: $(SP_LOGC_DIR) $(SP_LOGC_OBJS) $(SP_LOGC_LIB) $(SP_LOGC_STATIC)
 
 # Compiles and builds build/depends/log.c/log.o, depends on subproject srcs
 $(SP_LOGC_OBJS): $(SP_LOGC_SRCS)
-	@echo "Building $(SP_LOG_C_NAME):"
+	@echo "Compiling $(SP_LOG_C_NAME) sources"
 	$(CC) -c -fPIC $(TARGET_FLAGS) $(LOGC_FLAGS) -o $@ $^
-	@echo "Finished compiling $(SP_LOG_C_NAME)"
 
 # Links and creates dynamic library in build/depends/log.c/liblog.so
 $(SP_LOGC_LIB): $(SP_LOGC_OBJS)
+	@echo "Creating $(SP_LOG_C_NAME) shared library"
 	$(CC) -shared $(SP_INCLUDES) -o $@ $^
-	@echo "Created $(SP_LOG_C_NAME) shared library"
 
 # Links and creates static library in build/depends/log.c/liblog.so
 $(SP_LOGC_STATIC): $(SP_LOGC_OBJS)
+	@echo "Creating $(SP_LOG_C_NAME) static library"
 	ar rcs $@ $^
-	@echo "Created $(SP_LOG_C_NAME) static library"
 
 $(SP_LOGC_DIR):
 	$(MKDIR) $(SP_LOGC_DIR)
