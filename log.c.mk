116fdaea040c904b3d2c8ed16c74f6e66af15de2
Joseph Diza <josephm.diza@gmail.com>
2022-08-07 02:56:01 -0700

    Automatically install subproject headers

--- log.c.mk
+++ log.c.mk
@@ -5,6 +5,9 @@ SP_SRCS_logc 				= log.c
 SP_SRCD_logc				= $(SUBPROJECTS)/$(SP_NAME_$(1))/src
 SP_OBJS_logc 				= $(SP_SRCS_$(1):.c=.o)
 SP_INCS_logc 				= -I$(SP_SRCD_$(1))
+SP_HDRS_logc 				= log.h
+SP_HDRD_logc				= $(SP_SRCD_logc)
+#SP_HDRD_logc				= src
 
 # Build dynamic, static libraries
 SP_LIB_NAME_logc 		= logc
--- subproject.mk
+++ subproject.mk
@@ -1,6 +1,11 @@
 define subproject_template
 
-.PHONY: $(SP_TARGET_NAME_$(1)) clean-$(SP_TARGET_NAME_$(1))
+#.PHONY: $(SP_TARGET_NAME_$(1)) install-$(SP_TARGET_NAME_$(1)) uninstall-$(SP_TARGET_NAME_$(1)) clean-$(SP_TARGET_NAME_$(1))
+#.PHONY: $(SP_TARGET_NAME_$(1)) install-$(SP_TARGET_NAME_$(1))-headers uninstall-$(SP_TARGET_NAME_$(1))-headers clean-$(SP_TARGET_NAME_$(1))
+#.PHONY: $(SP_TARGET_NAME_$(1)) clean-$(SP_TARGET_NAME_$(1))
+
+
+.PHONY: $(SP_TARGET_NAME_$(1)) install-$(SP_TARGET_NAME_$(1))-headers uninstall-$(SP_TARGET_NAME_$(1))-headers clean-$(SP_TARGET_NAME_$(1))
 
 # SP_LOGC_DIR: Outputs build dependencies to build/{debug,release}/depends/subproject_name
 SP_BDIR_$(1) 	 	= $(TARGET_DIR)/depends/$(SP_NAME_$(1))
@@ -8,11 +13,63 @@ SP_BSRCS_$(1) 	= $(addprefix $(SP_SRCD_$(1))/, $(SP_SRCS_$(1)))
 SP_BOBJS_$(1) 	= $(addprefix $$(SP_BDIR_$(1))/, $(SP_OBJS_$(1)))
 SP_BCFLAGS_$(1) = $(SP_FLAGS_$(1)) $(SP_INCS_$(1))
 
+# Subproject headers
+
+#ifeq($(SP_HDRD_$(1)),)
+#SP_LIB_HDR_SRCS = $(addprefix $(PATHI)/, $(SP_HDRS_$(1)))
+#else
+#SP_LIB_HDR_SRCS = $(addprefix $(SP_HDRD_$(1))/, $(SP_HDRS_$(1)))
+#endif
+
+#$(if ifeq $(SP_HDRD_$(1)) "", \
+#SP_LIB_HDR_SRCS = $(addprefix $(PATHI)/, $(SP_HDRS_$(1))), \
+#SP_LIB_HDR_SRCS = $(addprefix $(SP_HDRD_$(1))/, $(SP_HDRS_$(1))))
+
+
+SP_LIB_HDR_SRCS = $(addprefix $(SP_HDRD_$(1))/, $(SP_HDRS_$(1)))
+SP_LIB_HDR_DEST = $(DESTDIR)$(PREFIX)/include/
+#SP_LIB_HDR_SRCS = $(addprefix $(PATHI)/, $(LIBRARY_HDRS))
+#SP_LIB_HDR_OUTS = $(DESTDIR)$(PREFIX)/include/$(LIBRARY_HDRS)
+SP_LIB_HDR_OUTS = $(DESTDIR)$(PREFIX)/include/$(SP_HDRS_$(1))
+
 # Add to subprojects
 SP_SOURCES 	+= $$(SP_BSRCS_$(1))
 SP_DEPENDS 	+= $$(SP_BOBJS_$(1))
 SP_INCLUDES += $(SP_INCS_$(1))
 SP_LIBS 		+= $(SP_SHARED_$(1))
+
+# Add install/uninstall targets to master subproject installs/uninstalls
+INSTALL_SP_TARGET += install-$(SP_TARGET_NAME_$(1))
+UNINSTALL_SP_TARGET += uninstall-$(SP_TARGET_NAME_$(1))
+
+# Create install targets
+
+# Install the library
+install-$(SP_TARGET_NAME_$(1)): $(SP_SHARED_$(1)) $(SP_STATIC_$(1)) install-$(SP_TARGET_NAME_$(1))-headers
+	install $(SP_SHARED_$(1)) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).so
+	install $(SP_STATIC_$(1)) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).a
+
+uninstall-$(SP_TARGET_NAME_$(1)): $(SP_SHARED_$(1)) $(SP_STATIC_$(1)) uninstall-$(SP_TARGET_NAME_$(1))-headers
+	$$(CLEANUP) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).so
+	$$(CLEANUP) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).a
+
+# Install the library headers
+install-$(SP_TARGET_NAME_$(1))-headers: $$(SP_LIB_HDR_OUTS)
+
+#$$(SP_LIB_HDR_DEST)/%.h: $()/%.h
+#$(DESTDIR)$(PREFIX)/include/%.h: $(PATHI)/%.h
+#$$(USR_INCLUDE)/%.h: $(PATHI)/%.h
+#SP_LIB_HDR_DEST/%.h
+#$$(USR_INCLUDE)/%.h: $$(PATHI)/%.h
+#$(USR_INCLUDE)/%.h: $(PATHI)/%.h
+#$(DESTDIR)$(PREFIX)/include/%.h: $(PATHI)/%.h
+
+$(USR_INCLUDE)/%.h: $(SP_HDRD_$(1))/%.h
+	install $$^ $$@
+
+
+uninstall-$(SP_TARGET_NAME_$(1))-headers: $$(SP_LIB_HDR_OUTS)
+	$$(CLEANUP) $$^
 
 # Create the directory, object files, dynamic & static libraries
 $(SP_TARGET_NAME_$(1)): $$(SP_BDIR_$(1)) $$(SP_BOBJS_$(1)) $(SP_SHARED_$(1)) $(SP_STATIC_$(1))
