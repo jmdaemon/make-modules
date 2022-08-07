e5630deb5df875ba0ab8071dda0a91a8b310e645
Joseph Diza <josephm.diza@gmail.com>
2022-08-07 04:53:55 -0700

    Use new subproject tempalte in tomlc99.mk

--- tomlc99.mk
+++ tomlc99.mk
@@ -1,51 +1,16 @@
-# Toml
+SP_TARGET_NAME_tomlc99 = tomlc99
 
-# Usage:
-# To include this makefile module into your project:
-# 1. Define SP_DEPENDS, SP_INCLUDES variables
-# 2. Add this line after their definitions:
-# 	include(tomlc99.mk)
-# 3. Add $(SP_DEPENDS) to your link targets like so:
-# 	bin: $(SP_DEPENDS)
-# 4. Add the headers in $(SP_INCLUDES) to your targets:
-# $(EXE_DEPS)/%.o: $(PATHS)/%.c $(SP_DEPENDS)
-#		@echo "Compiling main binary target source"
-#		$(CC) -c $(EXE_FLAGS) $(SP_INCLUDES) -o $@ $<
-# 5. Run your make commands to build your targets
+SP_NAME_tomlc99 				= tomlc99
+SP_SRCS_tomlc99 				= toml.c
+SP_SRCD_tomlc99					= $(SUBPROJECTS)/$(SP_NAME_$(1))
+SP_OBJS_tomlc99 				= $(SP_SRCS_$(1):.c=.o)
+SP_INCS_tomlc99 				= -I$(SUBPROJECTS)/$(SP_NAME_$(1))
+SP_HDRS_tomlc99 				= $(SP_SRCS_$(1):.c=.h)
+SP_HDRD_tomlc99					= $(SUBPROJECTS)/$(SP_NAME_$(1))
 
-# Variables
-SUB_TOML_NAME = tomlc99
-SUB_TOML_SRCS = toml.c
-SUB_TOML_OBJS = $(SUB_TOML_SRCS:.c=.o)
-SUB_TOML_DIR 	= $(SUBPROJECTS)/$(SUB_TOML_NAME)
-SUB_TOML_SRC 	= $(SUB_TOML_DIR)
-SUB_TOML_INCLUDES = -I$(SUB_TOML_DIR)/include
-
-# Setup
-.PHONY: toml clean-toml
-
-# Tomlc99
-SP_TOML_DIR 	 = $(PATHD)/$(SUB_TOML_NAME)
-SP_TOML_SRCS 	 = $(addprefix $(SUB_TOML_SRC)/, $(SUB_TOML_SRCS))
-SP_TOML_OBJS 	 = $(addprefix $(SP_TOML_DIR)/, $(SUB_TOML_OBJS))
-SP_TOML_CFLAGS = $(SUB_TOML_INCLUDES)
-
-# Add to subprojects
-SP_DEPENDS 	+= $(SP_TOML_OBJS)
-SP_INCLUDES += $(SUB_TOML_INCLUDES)
-
-# Create the toml output file
-toml: $(SP_TOML_DIR) $(SP_TOML_OBJS)
-
-# Build build/depends/toml/toml.o
-# Depend on the toml.c source
-# Note that we build it with -fPIC since we include it in our library
-$(SP_TOML_OBJS): $(SP_TOML_SRCS)
-	$(CC) -c $(LIB_CFLAGS) $(SP_TOML_CFLAGS) -o $@ $^
-
-$(SP_TOML_DIR):
-	$(MKDIR) $(SP_TOML_DIR)
-
-clean-toml:
-	@echo "Removing tomlc99 build output"
-	$(CLEANUP) $(SP_TOML_OBJS)
+# Build dynamic, static libraries
+SP_LIB_NAME_tomlc99 		= tomlc99
+#SP_SHARED_tomlc99				= $$(SP_BDIR_$(1))/lib$(SP_LIB_NAME_$(1)).so
+#SP_STATIC_tomlc99				= $$(SP_BDIR_$(1))/lib$(SP_LIB_NAME_$(1)).a
+$(eval $($(call shared_lib_tmpl,$(SP_LIB_NAME_tomlc99))))
+$(eval $($(call static_lib_tmpl,$(SP_LIB_NAME_tomlc99))))
