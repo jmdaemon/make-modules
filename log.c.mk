57d725dc9b2f8b961992af271cfb229451dca617
Joseph Diza <josephm.diza@gmail.com>
2022-08-06 23:32:38 -0700

    Remove dead makefile code

--- log.c.mk
+++ log.c.mk
@@ -1,4 +1,3 @@
-# Variables
 SP_TARGET_NAME_logc = logc
 
 SP_NAME_logc 				= log.c
@@ -11,13 +10,3 @@ SP_INCS_logc 				= -I$(SP_SRCD_$(1))
 SP_LIB_NAME_logc 		= logc
 SP_SHARED_logc 			= $$(SP_BDIR_$(1))/lib$(SP_LIB_NAME_$(1)).so
 SP_STATIC_logc 			= $$(SP_BDIR_$(1))/lib$(SP_LIB_NAME_$(1)).a
-
-# Include subproject_template
-#include make/subproject.mk
-
-#$(eval $(foreach subproject,$(SP_NAMES),$(call subproject_template,$(subproject))))
-#$(info $(foreach subproject,$(SP_NAMES),$(call subproject_template,$(subproject))))
-
-#SP_SHARED_logc 			= build/debug/depends/log.c/liblogc.so
-#SP_STATIC_logc 			= build/debug/depends/log.c/liblogc.a
-
--- utility.mk
+++ utility.mk
@@ -1,4 +1,3 @@
-# Variables
 SP_TARGET_NAME_utility = utility
 
 SP_NAME_utility 				= utility
@@ -10,9 +9,3 @@ SP_INCS_utility 				= -I$(SUBPROJECTS)/$(SP_NAME_$(1))/include
 SP_LIB_NAME_utility 		= utility
 SP_SHARED_utility 			= $$(SP_BDIR_$(1))/lib$(SP_LIB_NAME_$(1)).so
 SP_STATIC_utility 			= $$(SP_BDIR_$(1))/lib$(SP_LIB_NAME_$(1)).a
-
-#SP_SHARED_utility 			= build/debug/depends/utility/lib$(SP_LIB_NAME_$(1)).so
-#SP_STATIC_utility 			= build/debug/depends/utility/lib$(SP_LIB_NAME_$(1)).a
-
-#SP_SHARED_utility 			= build/debug/depends/utility.c/libutility.so
-#SP_STATIC_utility 			= build/debug/depends/utility.c/libutility.a
