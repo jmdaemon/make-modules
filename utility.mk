5b01b958c05ba28355af2f92570d68c9df61ad99
Joseph Diza <josephm.diza@gmail.com>
2022-08-07 03:44:24 -0700

    Install utility headers with makefile

--- utility.mk
+++ utility.mk
@@ -5,6 +5,8 @@ SP_SRCS_utility 				= utility.c command.c file.c
 SP_SRCD_utility					= $(SUBPROJECTS)/$(SP_NAME_$(1))/src
 SP_OBJS_utility 				= $(SP_SRCS_$(1):.c=.o)
 SP_INCS_utility 				= -I$(SUBPROJECTS)/$(SP_NAME_$(1))/include
+SP_HDRS_utility					= $(SP_SRCS_$(1):.c=.h)
+SP_HDRD_utility					= $(SUBPROJECTS)/$(SP_NAME_$(1))/include
 
 SP_LIB_NAME_utility 		= utility
 SP_SHARED_utility 			= $$(SP_BDIR_$(1))/lib$(SP_LIB_NAME_$(1)).so
