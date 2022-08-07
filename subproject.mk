06427cc1cdb1057dd5106e111e64ed60994fd595
Joseph Diza <josephm.diza@gmail.com>
2022-08-07 04:27:34 -0700

    Use new USR_INCLUDE,USR_LIB vars in subprojects module

--- subproject.mk
+++ subproject.mk
@@ -13,11 +13,9 @@ SP_BCFLAGS_$(1) = $(SP_FLAGS_$(1)) $(SP_INCS_$(1))
 ifneq ($(SP_HDRD_$(1)),)
 SP_LIB_INCLUDES_$(1) = $(SP_HDRD_$(1))
 SP_LIB_HDR_SRCS_$(1) = $(addprefix $(SP_HDRD_$(1))/, $(SP_HDRS_$(1)))
-#SP_LIB_HDR_OUTS_$(1) = $(USR_INCLUDE)/$(SP_HDRS_$(1))
 else
 SP_LIB_INCLUDES_$(1) = $(PATHI)
 SP_LIB_HDR_SRCS_$(1) = $(addprefix $(PATHI)/, $(SP_HDRS_$(1)))
-#SP_LIB_HDR_OUTS_$(1) = $(addprefix $(USR_INCLUDE)/, $(PATHI_$(1)))
 endif
 
 SP_LIB_HDR_OUTS_$(1) = $(addprefix $(USR_INCLUDE)/, $(SP_HDRS_$(1)))
@@ -36,12 +34,12 @@ UNINSTALL_SP_TARGET += uninstall-$(SP_TARGET_NAME_$(1))
 
 # Install the library
 install-$(SP_TARGET_NAME_$(1)): $(SP_SHARED_$(1)) $(SP_STATIC_$(1)) install-$(SP_TARGET_NAME_$(1))-headers
-	install $(SP_SHARED_$(1)) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).so
-	install $(SP_STATIC_$(1)) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).a
+	install $(SP_SHARED_$(1)) $$(USR_LIB)/lib$(SP_LIB_NAME_$(1)).so
+	install $(SP_STATIC_$(1)) $$(USR_LIB)/lib$(SP_LIB_NAME_$(1)).a
 
 uninstall-$(SP_TARGET_NAME_$(1)): $(SP_SHARED_$(1)) $(SP_STATIC_$(1)) uninstall-$(SP_TARGET_NAME_$(1))-headers
-	$$(CLEANUP) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).so
-	$$(CLEANUP) $$(DESTDIR)$$(PREFIX)/lib/lib$(SP_LIB_NAME_$(1)).a
+	$$(CLEANUP) $$(USR_LIB)/lib$(SP_LIB_NAME_$(1)).so
+	$$(CLEANUP) $$(USR_LIB)/lib$(SP_LIB_NAME_$(1)).a
 
 # Install the library headers
 install-$(SP_TARGET_NAME_$(1))-headers: $$(SP_LIB_HDR_OUTS_$(1))
